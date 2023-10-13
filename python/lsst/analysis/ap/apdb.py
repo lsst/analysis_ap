72bc3cf7c2b3b63d5d82933fcf02287b6c6aeab8
John Parejko <parejkoj@uw.edu>
2023-10-13 01:08:40 -0700

    Add min sources cut when loading diaObjects

--- python/lsst/analysis/ap/apdb.py
+++ python/lsst/analysis/ap/apdb.py
@@ -233,16 +233,19 @@ class DbQuery(abc.ABC):
             result = pd.read_sql_query(query, connection)
         return result.iloc[0]
 
-    def load_objects(self, limit=100000, latest=True):
+    def load_objects(self, limit=100000, latest=True, min_sources=None):
         """Load all diaObjects.
 
         Parameters
         ----------
         limit : `int`
             Maximum number of rows to return.
-        latest : `bool`
+        latest : `bool`, optional
             Only load diaObjects where validityEnd is None.
             These are the most-recently updated diaObjects.
+        min_sources : `int`, optional
+            Restrict to diaObjects that have at least this many sources
+            (nDiaSources > min_sources).
 
         Returns
         -------
@@ -252,6 +255,8 @@ class DbQuery(abc.ABC):
         table = self._tables["DiaObject"]
         if latest:
             query = table.select().where(table.columns["validityEnd"] == None)  # noqa: E711
+        if min_sources is not None:
+            query = query.where(table.columns["nDiaSources"] > min_sources)  # noqa: E711
         query = query.order_by(table.columns["diaObjectId"])
         if limit is not None:
             query = query.limit(limit)
